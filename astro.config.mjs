01f60b2c9d0b134cdf7779cb40c457922afaf41e
mooenz <joss92821@hotmail.com>
2025-12-06 15:07:50 -0500

    fix(config): cambiar el adaptador a vercel serverless y actualizar la salida a 'server'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-	site: 'https://patio-la-boca.vercel.app/',
-	base: '/',
-	output: 'static',
-	adapter: vercel({}),
-	i18n: {
-		defaultLocale: 'es',
-		locales: ['es', 'en'],
-		routing: {
-			prefixDefaultLocale: false,
-		},
-	},
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  site: 'https://patio-la-boca.vercel.app/',
+  base: '/',
+  output: 'server',
+  adapter: vercel({}),
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
+
